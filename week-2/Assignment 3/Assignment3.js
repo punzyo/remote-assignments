6740126053a0a2e7c63afc11fa4c4ebd7a11cce8
punzyo <147231492+punzyo@users.noreply.github.com>
2024-01-24 12:49:17 +0800

    改檔名

--- week-2/Assignment 3/Assignment3.js
+++ week-2/Assignment 3/Assignment3.js
@@ -1,26 +1,26 @@
 function calculate(data) {
-    const {discount, products} = data;
-    let totalPrice = 0;
-    for (const product of products) {
-        totalPrice += product.price;
-    }
-    return totalPrice * discount;
+  const { discount, products } = data;
+  let totalPrice = 0;
+  for (const product of products) {
+    totalPrice += product.price;
+  }
+  return totalPrice * discount;
 }
 const discountedPrice = calculate({
-    discount: 0.1,
-    products: [
+  discount: 0.1,
+  products: [
     {
-    name: "Product 1",
-    price: 100
+      name: "Product 1",
+      price: 100,
     },
     {
-    name: "Product 2",
-    price: 700
+      name: "Product 2",
+      price: 700,
     },
     {
-    name: "Product 3",
-    price: 250
-    }
-    ]
+      name: "Product 3",
+      price: 250,
+    },
+  ],
 });
-    console.log(discountedPrice) // show the total price of all products after applying a discount
+console.log(discountedPrice); // show the total price of all products after applying a discount
