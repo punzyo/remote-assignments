fe81437b996c48eb469c0df6bad56a9385272001
Yi-li Lin <147231492+punzyo@users.noreply.github.com>
2024-04-06 19:56:24 +0800

    proxy

--- week-3/Assignments/server.js
+++ week-3/Assignments/server.js
@@ -1,65 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const https = require('https');
+
 const app = express();
-const cookieParsar = require('cookie-parser');
 
-app.use(cookieParsar());
+// 使用 CORS 中間件，允許所有來源的請求
+app.use(cors());
 
-app.use(express.static('public'));
-
-app.set('view engine', 'pug');
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/getData', (req, res) => {
-  let { number } = req.query;
-  if (number > 0) {
-    number = (parseInt(number) * (parseInt(number) + 1)) / 2;
-    //res.render('sum', {number});
-    res.json({ number });
-  } else if (!number) {
-    res.json('Lack of Parameter');
-  } else {
-    res.json('Wrong Parameter');
+// 設定一個路由處理 GET 請求
+app.get('/api/proxy', async (req, res) => {
+  try {
+    // 在這裡指定你想要代理的目標網址
+    const targetUrl = 'https://rate.bot.com.tw/xrt?Lang=zh-TW';
+    
+    // 使用 Axios 進行 HTTPS 請求，並設置合適的代理
+    const response = await axios.get(targetUrl, {
+      httpsAgent: new https.Agent({ rejectUnauthorized: false })
+    });
+    
+    // 將目標伺服器的回應返回給請求方
+    res.json(response.data);
+  } catch (error) {
+    // 如果出現錯誤，返回錯誤訊息
+    console.error('Proxy request error:', error);
+    res.status(500).json({ error: 'Proxy request failed' });
   }
 });
 
-app.get('/myName', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('showName', { name });
-  } else {
-    res.render('trackName');
-  }
+// 監聽在指定的端口上
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
 });
-app.get('/trackName', (req, res) => {
-  const { name } = req.query;
-  res.cookie('username', name);
-  res.redirect('/myName');
-});
-app.post('/trackName', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/myName');
-});
-
-app.use((req, res, next) => {
-  const err = new Error('此路徑不存在');
-  err.status = 404;
-  next(err);
-});
-
-app.use((req, res, next) => {
-  const err = new Error('其他錯誤');
-  err.status = 500;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render('err');
-});
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
