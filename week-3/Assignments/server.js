5cd4cc56bf69ed82e3d54b96c0877f106c7aa0eb
Yi-li Lin <147231492+punzyo@users.noreply.github.com>
2024-04-09 12:36:41 +0800

    Update server.js

--- week-3/Assignments/server.js
+++ week-3/Assignments/server.js
@@ -1,42 +1,43 @@
-// 使用 ES Module 语法导入模块
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-const app = express();
 
-app.use(cors()); // 使用默认配置，允许所有跨域请求
+const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*", // 允许任何来源
-        methods: ["GET", "POST"] // 允许的 HTTP 方法
+        origin: "*", // 根据实际情况调整
+        methods: ["GET", "POST"]
     }
 });
 
 io.on('connection', socket => {
     console.log('A user connected:', socket.id);
 
+    // 当用户加入房间时
     socket.on('join', room => {
         socket.join(room);
         console.log(`User ${socket.id} joined room ${room}`);
 
-        // 当用户加入房间时，向房间内的其他用户广播
+        // 向房间内的其他用户广播新用户加入
         socket.to(room).emit('user joined', socket.id);
 
-        // 当有信令数据需要交换时
-        socket.on('signal', ({ room, data }) => {
-            // 广播信令数据给房间内的其他用户，除了发送者自己
-            socket.to(room).emit('signal', { id: socket.id, signal: data });
+        // 当收到信号时，将其转发给房间内的其他用户
+        socket.on('signal', ({ userToSignal, callerID, signal }) => {
+            io.to(userToSignal).emit('signal', { id: callerID, signal });
         });
 
+        // 当用户离开时
         socket.on('disconnect', () => {
             console.log('User disconnected:', socket.id);
+            // 向房间内的其他用户广播用户离开的消息
+            socket.to(room).emit('user left', socket.id);
         });
     });
 });
-
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
