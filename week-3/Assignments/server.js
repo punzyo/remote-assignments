e3f7ea02046dcddb109b0df6def4cb4dfd7aea93
Yi-li Lin <147231492+punzyo@users.noreply.github.com>
2024-04-09 13:14:02 +0800

    Update server.js

--- week-3/Assignments/server.js
+++ week-3/Assignments/server.js
@@ -17,29 +17,33 @@ const io = new Server(server, {
 io.on('connection', socket => {
     console.log('A user connected:', socket.id);
 
-    socket.on('join', room => {
-        // 将新用户加入指定的房间
+    socket.on('join room', room => {
         socket.join(room);
         console.log(`User ${socket.id} joined room ${room}`);
 
         // 获取房间内所有已经存在的Socket连接（不包括自己）
-        const otherUsers = io.sockets.adapter.rooms.get(room);
-        const usersToSend = Array.from(otherUsers).filter(id => id !== socket.id);
+        const otherUsers = io.sockets.adapter.rooms.get(room)?.size ? Array.from(io.sockets.adapter.rooms.get(room)) : [];
+        const usersToSend = otherUsers.filter(id => id !== socket.id);
 
         // 向加入的新用户发送房间内所有已经存在的用户列表
         socket.emit('all users', usersToSend);
 
-        // 当收到信号时，将其转发给房间内的指定用户
-        socket.on('signal', ({ userToSignal, callerID, signal }) => {
-            io.to(userToSignal).emit('signal', { id: callerID, signal });
-        });
+        // 通知房间内的其他用户有新用户加入
+        socket.to(room).emit('user joined', socket.id);
+    });
 
-        // 当用户离开时
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-            // 向房间内的其他用户广播用户离开的消息
-            socket.to(room).emit('user left', socket.id);
-        });
+    socket.on('sending signal', payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on('returning signal', payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        // 向所有客户端广播用户离开的消息
+        io.emit('user left', socket.id);
     });
 });
 
@@ -47,3 +51,4 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
